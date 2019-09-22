4ceee251d9773f693806cb9874a25d828c6947c2
shayankhanani <shayan.khanani@outlook.com>
2019-09-22 22:55:02 +0500

    ES6 or ES2015 or Harmony is a modern update of js
    let and const are new ways of declaring variable
    
    let and const are bound inside {} temporal dead zone and can not be
    hoisted like var
    
    let can be re-assigned within a scope but const cant
    
    the code gives example of let and const along with some example that
    will generate error because of the scope of let and const and const
    re-assignment within a scope.

---
+++ let-const.js
@@ -0,0 +1,34 @@
+function getNumber(x){
+    if(x){
+        var number = 3;
+        console.log(number);
+    }
+    else{
+        console.log("not a :" + number);
+    }
+}
+
+function getNumber2(x){
+    if(x){
+        let number = 3;
+        console.log(number);
+        number = 6;
+        console.log(number);
+    }
+    else{
+        console.log("not a :" + number);
+    }
+}
+
+function getNumber3(x){
+    if(x){
+        const number = 9;
+        console.log(number);
+        number = 1;
+        console.log(number);
+    }
+    else{
+        console.log("not a : "+ number);
+    }
+}
+
